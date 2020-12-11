d22c031af8d2619011840cdce4fdfd42efdb95f1
Jeff Lewis <jeffylew77@gmail.com>
2020-12-10 18:00:11 -0800

    [Dev] Adding subtraction tests

--- src/__tests__/mathExact.test.tsx
+++ src/__tests__/mathExact.test.tsx
@@ -56,4 +56,55 @@ describe('mathExact', () => {
 
 
   // MATH TYPE: SUBTRACT
+  // Test #11
+  test('Test #11', () => {
+    expect(mathExact('Subtract', 1, 2)).toEqual(-1);
+  });
+
+  // Test #12
+  test('Test #12', () => {
+    expect(mathExact('Subtract', 10, 20)).toEqual(-10);
+  });
+
+  // Test #13
+  test('Test #13', () => {
+    expect(mathExact('Subtract', 100, 200)).toEqual(-100);
+  });
+
+  // Test #14
+  test('Test #14', () => {
+    expect(mathExact('Subtract', .1, .2)).toEqual(-.1);
+  });
+
+  // Test #15
+  test('Test #15', () => {
+    expect(mathExact('Subtract', 1.123, .002)).toEqual(1.121);
+  });
+
+  // Test #16
+  test('Test #16', () => {
+    expect(mathExact('Subtract', 1.123, .02)).toEqual(1.103);
+  });
+
+  // Test #17
+  test('Test #17', () => {
+    expect(mathExact('Subtract', 1.123, .2)).toEqual(0.923);
+  });
+
+  // Test #18
+  test('Test #18', () => {
+    expect(mathExact('Subtract', .002, 1.123)).toEqual(-1.121);
+  });
+
+  // Test #19
+  test('Test #19', () => {
+    expect(mathExact('Subtract', .02, 1.123)).toEqual(-1.103);
+  });
+
+  // Test #20
+  test('Test #20', () => {
+    expect(mathExact('Subtract', .2, 1.123)).toEqual(-.923);
+  });
+
+
 });
